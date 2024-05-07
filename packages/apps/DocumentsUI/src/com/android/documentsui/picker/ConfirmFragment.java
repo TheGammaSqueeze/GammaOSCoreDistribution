90083e2c8c91e76e7d487011d6c3627218696a5c
TheGammaSqueeze <thegammasqueeze@gmail.com>
2024-05-07 16:12:55 +0000

    DocumentsUI: Autoconfirm folder selection

--- packages/apps/DocumentsUI/src/com/android/documentsui/picker/ConfirmFragment.java
+++ packages/apps/DocumentsUI/src/com/android/documentsui/picker/ConfirmFragment.java
@@ -1,28 +1,15 @@
-/*
- * Copyright (C) 2019 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.android.documentsui.picker;
-
-import static com.android.documentsui.base.Shared.getCallingAppName;
 
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -32,12 +19,8 @@ import com.android.documentsui.BaseActivity;
 import com.android.documentsui.R;
 import com.android.documentsui.base.DocumentInfo;
 import com.android.documentsui.base.Shared;
-
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
-/**
- * Used to confirm with user that it's OK to overwrite an existing file.
- */
 public class ConfirmFragment extends DialogFragment {
 
     private static final String TAG = "ConfirmFragment";
@@ -54,59 +37,84 @@ public class ConfirmFragment extends DialogFragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        mActions = ((PickActivity) getActivity()).getInjector().actions;
+        PickActivity pickActivity = (PickActivity) getActivity();
+        if (pickActivity != null) {
+            mActions = pickActivity.getInjector().actions;
+        }
     }
 
+    @NonNull
     @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         Bundle arg = (getArguments() != null) ? getArguments() : savedInstanceState;
 
         mTarget = arg.getParcelable(Shared.EXTRA_DOC);
         mType = arg.getInt(CONFIRM_TYPE);
-        final PickResult pickResult = ((PickActivity) getActivity()).getInjector().pickResult;
+        final PickActivity pickActivity = (PickActivity) getActivity();
+        final PickResult pickResult = pickActivity != null ? pickActivity.getInjector().pickResult : null;
 
-        final MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(getActivity());
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(getActivity());
+        String message;
         switch (mType) {
             case TYPE_OVERWRITE:
-                String message = String.format(
-                        getString(R.string.overwrite_file_confirmation_message),
-                        mTarget.displayName);
+                message = String.format(getString(R.string.overwrite_file_confirmation_message), mTarget.displayName);
                 builder.setMessage(message);
-                builder.setPositiveButton(
-                        android.R.string.ok,
-                        (DialogInterface dialog, int id) -> {
-                            pickResult.increaseActionCount();
-                            mActions.finishPicking(mTarget.getDocumentUri());
-                        });
+                builder.setPositiveButton(android.R.string.ok, (DialogInterface dialog, int id) -> {
+                    if (pickResult != null) {
+                        pickResult.increaseActionCount();
+                    }
+                    if (mActions != null) {
+                        mActions.finishPicking(mTarget.getDocumentUri());
+                    }
+                });
                 break;
             case TYPE_OEPN_TREE:
                 final Uri treeUri = mTarget.getTreeDocumentUri();
                 final BaseActivity activity = (BaseActivity) getActivity();
-                final String target = activity.getCurrentTitle();
-                final String text = getString(R.string.open_tree_dialog_title,
-                        getCallingAppName(getActivity()), target);
-                message = getString(R.string.open_tree_dialog_message,
-                        getCallingAppName(getActivity()), target);
+                final String target = activity != null ? activity.getCurrentTitle() : null;
+                final String callingAppName = getCallingAppName(activity);
+                final String text = getString(R.string.open_tree_dialog_title, callingAppName, target);
+                message = getString(R.string.open_tree_dialog_message, callingAppName, target);
 
                 builder.setTitle(text);
                 builder.setMessage(message);
-                builder.setPositiveButton(
-                        R.string.allow,
-                        (DialogInterface dialog, int id) -> {
-                            pickResult.increaseActionCount();
-                            mActions.finishPicking(treeUri);
-                        });
+                builder.setPositiveButton(R.string.allow, (DialogInterface dialog, int id) -> {
+                    if (pickResult != null) {
+                        pickResult.increaseActionCount();
+                    }
+                    if (mActions != null) {
+                        mActions.finishPicking(treeUri);
+                    }
+                });
                 break;
-
         }
-//        builder.setNegativeButton(android.R.string.cancel,
-//                (DialogInterface dialog, int id) -> pickResult.increaseActionCount());
 
-        return builder.create();
+        AlertDialog dialog = builder.create();
+        dialog.setOnShowListener(d -> {
+            // Automatically click the positive button when the dialog is shown
+            dialog.getButton(DialogInterface.BUTTON_POSITIVE).performClick();
+        });
+
+        return dialog;
+    }
+
+    private String getCallingAppName(BaseActivity activity) {
+        if (activity == null) {
+            return "";
+        }
+
+        try {
+            String packageName = activity.getCallingPackage();
+            PackageManager packageManager = activity.getPackageManager();
+            ApplicationInfo appInfo = packageManager.getApplicationInfo(packageName, 0);
+            return (String) packageManager.getApplicationLabel(appInfo);
+        } catch (PackageManager.NameNotFoundException e) {
+            return "";
+        }
     }
 
     @Override
-    public void onSaveInstanceState(Bundle outState) {
+    public void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
 
         outState.putParcelable(Shared.EXTRA_DOC, mTarget);
