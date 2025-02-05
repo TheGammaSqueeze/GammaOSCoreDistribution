a7ccb817a0e9689946bfdae03b14347be1549e3b
TheGammaSqueeze <thegammasqueeze@gmail.com>
2025-02-05 00:36:38 +0000

    Fix rgp2xbox permissions issue

--- device/google/atv/gammaos/rgp2xbox/rgp2xbox.sh
+++ device/google/atv/gammaos/rgp2xbox/rgp2xbox.sh
@@ -2,12 +2,12 @@
 
 if [ -x /vendor/bin/rgp2xbox ]; then
     # Create the destination directory if it doesn't exist
-    mkdir -p /data/rgp2xbox
+    mkdir -p /data/rgp2xbox_bin
 
     # Copy the file to /data/rgp2xbox preserving permissions
-    cp -p /vendor/bin/rgp2xbox /data/rgp2xbox/rgp2xbox
+    cp -p /vendor/bin/rgp2xbox /data/rgp2xbox_bin/rgp2xbox
 
-    CMD="/data/rgp2xbox/rgp2xbox"
+    CMD="/data/rgp2xbox_bin/rgp2xbox"
 else
     CMD="/system/bin/rgp2xbox"
 fi
