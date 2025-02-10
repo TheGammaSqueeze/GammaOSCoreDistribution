bd08fa61916c3be7a8a45523fe6a4348846a2da6
TheGammaSqueeze <thegammasqueeze@gmail.com>
2025-02-10 21:32:39 +0000

    Disable boringssl self test, speeds up allwinner boot

--- external/boringssl/selftest/boringssl_self_test.cpp
+++ external/boringssl/selftest/boringssl_self_test.cpp
@@ -21,11 +21,5 @@
 // See init.rc for details.
 // It may also exit before reaching main() if BoringSSL fast tests fail.
 int main(int, char**) {
-    if (!FIPS_mode()) {
-        return 1;  // Fail: BoringSSL not built in FIPS mode.
-    }
-    if (!BORINGSSL_self_test()) {
-        return 1;  // Fail: One or more self tests failed.
-    }
     return 0;      // Success
 }
